74144aa29310100b71bc194e85b4255478b93483
RafaelZelak <rzf1503@gmail.com>
2024-06-06 16:06:48 -0300

    Adicionando arquivos para a nova versão v2.0

--- disk_ui.py
+++ disk_ui.py
@@ -1,52 +1,122 @@
-# -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import messagebox
 import sip_module as sip
 import pjsua2 as pj
 import os
+from PIL import Image, ImageTk
+import asyncio
 
-# Definir o limite de caracteres
+# Variaveis
 root = tk.Tk()
 root.title("Fazer Chamada SIP")
 fonte = ("Roboto", 20)
-
 max_caracteres = 14
-
-# Variavel global para rastrear o estado da chamada
 is_calling = False
 call = None
+status = "#8a7070"
+call_state_var = False
 
-# Função para encerrar a chamada
-def end_call():
+# Evento para sinalizar que a janela foi fechada
+close_event = asyncio.Event()
+
+def verifica_conexao():
+    try:
+        with open('estado_ligacao.txt', 'r') as arquivo:
+            conteudo = arquivo.read().strip()
+            if conteudo == 'conectada':
+                return True
+            elif conteudo == 'desconectada':
+                return False
+            else:
+                raise ValueError("O conteúdo do arquivo não é válido. Esperado 'conectada' ou 'desconectada'.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return False
+    except ValueError as e:
+        print(e)
+        return False
+
+def draw_circle(canvas, x, y, radius):
+    global status
+    canvas.create_oval(x - radius, y - radius, x + radius, y + radius, outline=status, width=2, fill=status)
+
+async def check_call_status():
     global is_calling, call
-    if is_calling:
-        prm = pj.CallOpParam()
-        call.hangup(prm)  # Encerra a chamada usando o metodo hangup()
-        call_button.config(text="Ligar", bg="#718a70")
-        is_calling = False
+    while not close_event.is_set():
+        if verifica_conexao() == False:  # Verifica se a conexão está ativa
+            call_button.config(text="Ligar", bg="#718a70")
+            is_calling = False
+        await asyncio.sleep(.5)
+
+async def check_config_status():
+    global status
+
+    while not close_event.is_set():
+        try:
+            with open('config.cfg', 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    if line.startswith("login:"):
+                        login_status = line.split(":")[-1].strip()
+                        if login_status == '1':
+                            status = "#6A9D69"
+                        elif login_status == '0':
+                            status = "#9D5F5F"
+                        else:
+                            print("Status inválido no arquivo.")
+                        break
+                else:
+                    print("Login não encontrado no arquivo de configuração.")
+        except FileNotFoundError:
+            print("Arquivo de configuração não encontrado.")
+
+        # Atualizar o círculo no canvas
+        canvas.delete("all")  # Limpar o canvas
+        draw_circle(canvas, x, y, radius)
+        await asyncio.sleep(2)  # Aguardar 2 segundos antes de verificar novamente
+
 
 # Modificar a função para aceitar um argumento de evento
 def on_call_button_click(event=None):
     global is_calling, call
+
     destination_number = entry_var.get().replace("-", "").replace("(", "").replace(")", "")
     if destination_number[2] in ['6', '7', '8', '9']:
         if len(destination_number) == 10:
             destination_number = destination_number[:2] + '9' + destination_number[2:]
-            
-    if not is_calling:
+
+    if verifica_conexao() == False:
         if destination_number:
             try:
-                call = sip.make_call(account, destination_number)
-                # Mudar texto do botão e atualizar estado da chamada
-                call_button.config(text="Encerrar", bg="#8a7070")
-                is_calling = True
+                if not is_calling:  # Se não estiver em uma chamada, faça uma nova chamada
+                    call = sip.make_call(account, destination_number)
+                    call_button.config(text="Encerrar", bg="#8a7070")
+                    is_calling = True
+                else:  # Se já estiver em uma chamada, encerre-a
+                    prm = pj.CallOpParam()
+                    call.hangup(prm) 
+                    call_button.config(text="Ligar", bg="#718a70")
+                    is_calling = False
             except Exception as e:
-                messagebox.showerror("Erro", f"Erro ao fazer a chamada: {e}")
+                if not is_calling:
+                    messagebox.showerror("Erro", f"Erro ao fazer a chamada: {e}")
+                else:
+                    # Se já estiver em uma chamada e ocorrer um erro, trate o erro aqui
+                    call_button.config(text="Ligar", bg="#718a70")
+                    is_calling = False
         else:
             messagebox.showwarning("Aviso", "Por favor, insira um número de destino.")
     else:
         # Se já estiver em uma chamada, encerre-a
-        end_call()
+        if is_calling:
+            prm = pj.CallOpParam()
+            call.hangup(prm) 
+            call_button.config(text="Ligar", bg="#718a70")
+            is_calling = False
+        else:
+            messagebox.showwarning("Aviso", "Você está conectado. Não é possível fazer uma nova chamada.")
+
+
 
 def select_character():
     entry_widget.after_idle(lambda: entry_widget.icursor(tk.END))
@@ -57,9 +127,9 @@ def formatar_numero(entrada):
         entrada = f"({entrada[:2]}){entrada[2:]}"  # Adiciona parênteses nos primeiros dois dígitos
     if '-' not in entrada:
         if len(entrada) > 8:
-            entrada = f"{entrada[:8]}-{entrada[8:]}"  # Adiciona o hífen após o sexto dígito    
+            entrada = f"{entrada[:8]}-{entrada[8:]}"  # Adiciona o hífen após o sexto dígito
             if '-' in entrada and len(entrada) == 14:
-                entrada = entrada.replace('-', '')  
+                entrada = entrada.replace('-', '')
                 entrada = f"{entrada[:9]}-{entrada[9:]}"
     if len(entrada) != 3:
         entry_widget.after_idle(lambda: entry_widget.icursor(3))
@@ -88,7 +158,7 @@ def limitar_caracteres(*args):
         label_baixo.config(text="Sem formato")
 
     entry_text = formatar_numero(entry_text)
-        
+
     entry_var.set(entry_text)
 
 def on_number_button_click(number):
@@ -97,14 +167,48 @@ def on_number_button_click(number):
     entry_widget.delete(0, tk.END)
     entry_widget.insert(0, new_text)
 
+
 # Função para trocar para a tela de configurações
 def show_config_screen():
+    tooltip.destroy()
     os.system("python3 config_ui.py")
 
+img_config = Image.open("img/config_ico.png")
+img_config = img_config.resize((25, 25))
+img_config_tk = ImageTk.PhotoImage(img_config)
+config_button = tk.Button(root, command=show_config_screen, image=img_config_tk, borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0, bg="#333333", activebackground="#333333")
+config_button.place(relx=0.95, rely=0.03, anchor='center', relwidth=0.1, relheight=0.06)
 
+img_hist = Image.open("img/hist_ico.png")
+img_hist = img_hist.resize((25, 25))
+img_hist_tk = ImageTk.PhotoImage(img_hist)
+hist_button = tk.Button(root, command=show_config_screen, image=img_hist_tk, borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0, bg="#333333", activebackground="#333333")
+hist_button.place(relx=0.95, rely=0.102, anchor='center', relwidth=0.1, relheight=0.06)
 
-config_button = tk.Button(root, command=show_config_screen, borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0)
-config_button.place(relx=0.90, rely=0.06, anchor='center', relwidth=0.1, relheight=0.06)
+# Define os textos das dicas de ferramentas
+config_tooltip_text = "Configurações"
+hist_tooltip_text = "Histórico"
+
+# Define as funções de exibição e ocultação de dicas de ferramentas
+def show_tooltip(event, button, tooltip_text):
+    global tooltip  # Declara tooltip como uma variável global
+
+    # Cria uma janela pop-up com o texto da dica de ferramenta
+    tooltip = tk.Toplevel(root)
+    tooltip.wm_overrideredirect(True)
+    tooltip.wm_geometry("+%d+%d" % (event.x_root + 5, event.y_root + 5))  # Define manualmente a posição da janela pop-up
+    label = tk.Label(tooltip, text=tooltip_text, background="#ffffff", foreground="#000000")
+    label.pack()
+
+def hide_tooltip(event):
+    tooltip.destroy()
+
+# Vincula as funções de exibição e ocultação de dicas de ferramentas aos eventos de entrada e saída do mouse
+config_button.bind("<Enter>", lambda event: show_tooltip(event, config_button, config_tooltip_text))
+config_button.bind("<Leave>", hide_tooltip)
+
+hist_button.bind("<Enter>", lambda event: show_tooltip(event, hist_button, hist_tooltip_text))
+hist_button.bind("<Leave>", hide_tooltip)
 
 label = tk.Label(root, text="Número de Destino:", bg="#333333", fg="#F0F0F0")
 label.place(relx=0.5, rely=0.05, anchor='center')
@@ -122,6 +226,14 @@ entry_widget.bind("<Return>", on_call_button_click)
 label_baixo = tk.Label(root, text="Sem formato", bg="#333333", fg="#F0F0F0")
 label_baixo.place(relx=0.5, rely=0.25, anchor='center')
 
+canvas = tk.Canvas(root, width=20, height=20, bg="#333333", borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0)
+canvas.pack()
+canvas.place(relx=0.04, rely=0.974, anchor='center')
+
+x = 10
+y = 10
+radius = 7
+
 # Teclado numérico
 buttons = [
     ('1', 0.25, 0.34), ('2', 0.5, 0.34), ('3', 0.75, 0.34),
@@ -134,10 +246,46 @@ for (text, relx, rely) in buttons:
     button = tk.Button(root, text=text, command=lambda t=text: on_number_button_click(t), bg="#555555", fg="#F0F0F0", highlightthickness=0, highlightbackground="#333333", bd=0)  # Remoção da borda
     button.place(relx=relx, rely=rely, anchor='center', relwidth=0.235, relheight=0.1)
 
-call_button = tk.Button(root, text="Ligar", command=on_call_button_click, bg="#718a70", fg="#F0F0F0", borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0)  
+call_button = tk.Button(root, text="Ligar", command=on_call_button_click, bg="#718a70", fg="#F0F0F0", borderwidth=0, highlightthickness=0, highlightbackground="#333333", bd=0)
 call_button.place(relx=0.5, rely=0.9, anchor='center', relwidth=0.78, relheight=0.1)
 
+# Função para limpar recursos SIP
+def clean_up():
+    global ep, transport, account, call
+    try:
+        if call:
+            call.hangup(pj.CallOpParam())
+            call = None
+    except Exception as e:
+        print(f"Erro ao encerrar a chamada: {e}")
+    try:
+        if account:
+            account.delete()
+            account = None
+    except Exception as e:
+        print(f"Erro ao limpar a conta: {e}")
+    try:
+        if transport:
+            transport = None
+    except Exception as e:
+        print(f"Erro ao limpar o transporte: {e}")
+    try:
+        if ep:
+            ep.libDestroy()
+            ep = None
+    except Exception as e:
+        print(f"Erro ao destruir o endpoint: {e}")
 
+# Capturar o evento de fechamento da janela para limpar recursos
+def on_close_window():
+    print("Limpando recursos SIP...")
+    clean_up()
+    print("Destruindo endpoint SIP...")
+    close_event.set()  # Sinaliza o fechamento da janela
+    root.destroy()
+    os._exit(0)
+
+root.protocol("WM_DELETE_WINDOW", on_close_window)
 
 # Configuração SIP
 ep, transport = sip.create_transport()
@@ -149,4 +297,24 @@ else:
     altura = 500
     root.configure(bg="#333333")
     root.geometry(f"{largura}x{altura}")
-    root.mainloop()
+
+    # Função para integrar o loop do Tkinter com o asyncio
+    async def main_loop():
+        asyncio.create_task(check_config_status())  # Iniciar a tarefa assíncrona dentro do loop
+        asyncio.create_task(check_call_status())  # Iniciar a tarefa assíncrona para verificar o status da chamada
+        while not close_event.is_set():
+            if not root.winfo_exists():  # Verifica se a janela ainda existe
+                break
+            root.update()
+            try:
+                ep.libHandleEvents(50)  # Manipular eventos SIP
+            except pj.Error as e:
+                print(f"Erro ao manipular eventos SIP: {e}")
+            await asyncio.sleep(0.01)
+
+    # Dentro do loop principal
+    try:
+        asyncio.run(main_loop())
+    finally:
+        clean_up()
+
--- sip_module.py
+++ sip_module.py
@@ -1,14 +1,22 @@
-# -*- coding: utf-8 -*-
-# sip_module.py
 import pjsua2 as pj
 import socket
 
+# Variável global para o endpoint
+ep = None
+call_onoff = False
+incoming_call = None
+
+# Função para ler configurações do arquivo
 def ler_configuracoes_do_arquivo(arquivo):
     configuracoes = {}
     with open(arquivo, 'r') as file:
         for line in file:
-            key, value = line.strip().split(': ')
-            configuracoes[key.strip()] = value.strip()
+            line = line.strip()
+            if ': ' in line:
+                key, value = line.split(': ', 1)
+                configuracoes[key.strip()] = value.strip()
+            else:
+                print(f"Linha ignorada por estar malformada: {line}")
     return configuracoes
 
 # Lendo as configurações do arquivo
@@ -25,26 +33,57 @@ PASSWORD = configuracoes.get('Password')
 # Lista de codecs a serem utilizados
 CODECS = ["PCMA/8000"]
 
+# Função para atualizar o estado da ligação em um arquivo
+def atualizar_estado_ligacao(estado):
+    with open('estado_ligacao.txt', 'w') as file:
+        file.write(estado)
+
+# Classe para representar uma chamada
 class MyCall(pj.Call):
     def __init__(self, account, call_id=pj.PJSUA_INVALID_ID):
         pj.Call.__init__(self, account, call_id)
     
     def onCallState(self, prm):
+        global call_info
         call_info = self.getInfo()
-        print("Call state changed to", call_info.stateText)
+        print(f"Call state changed to {call_info.stateText} for call ID: {self.getId()}")
         if call_info.state == pj.PJSIP_INV_STATE_DISCONNECTED:
             print("Call disconnected")
-            
-    def onCallMediaState(self, prm, ep):
-        call_info = self.getInfo()
-        for mi in call_info.media:
-            if mi.type == pj.PJMEDIA_TYPE_AUDIO and (mi.status == pj.PJSUA_CALL_MEDIA_ACTIVE):
-                media = self.getMedia(mi.index)
-                audio_media = pj.AudioMedia.typecastFromMedia(media)
-                ep.audDevManager().getCaptureDevMedia().startTransmit(audio_media)
-                ep.audDevManager().getPlaybackDevMedia().startTransmit(audio_media)
-                print("Audio transmission started")
+            atualizar_estado_ligacao('desconectada')
+            global incoming_call
+            incoming_call = None
 
+        elif call_info.state == pj.PJSIP_INV_STATE_CONFIRMED:
+            print("Call connected")
+            atualizar_estado_ligacao('conectada')
+
+    def onCallMediaState(self, prm):
+        global call_onoff
+        call_info = self.getInfo()
+        for media_info in call_info.media:
+            if media_info.type == pj.PJMEDIA_TYPE_AUDIO:
+                if media_info.status == pj.PJSUA_CALL_MEDIA_ACTIVE:
+                    print("Call media active")
+                    call_onoff = True
+                    # Corrigindo a chamada para getAudioMedia() com índice 0
+                    media = self.getAudioMedia(0)
+                    if media:
+                        # Usando a variável global ep
+                        media.startTransmit(ep.audDevManager().getCaptureDevMedia())
+                        ep.audDevManager().getPlaybackDevMedia().startTransmit(media)
+                elif media_info.status == pj.PJSUA_CALL_MEDIA_REMOTE_HOLD:
+                    print("Remote hold")
+                    self.answer()
+                elif media_info.status == pj.PJSUA_CALL_MEDIA_ERROR:
+                    print("Error in call media")
+                    self.hangup()
+
+    def answer(self):
+        prm = pj.CallOpParam()
+        prm.statusCode = pj.PJSIP_SC_OK
+        super(MyCall, self).answer(prm)
+
+# Classe para representar uma conta
 class MyAccount(pj.Account):
     def __init__(self):
         pj.Account.__init__(self)
@@ -52,25 +91,57 @@ class MyAccount(pj.Account):
     def onRegState(self, prm):
         if prm.code == 200:
             print("Registrado com sucesso!")
+            atualizar_login_status(1)
         else:
             print("Falha no registro.")
+            print(f"Código de erro: {prm.code}")
+            print(f"Razão da falha: {prm.reason}")
+            atualizar_login_status(0)
+    
+    def onIncomingCall(self, prm):
+        global incoming_call
+        call_id = prm.callId
+        atualizar_estado_ligacao('conectada')
+        print("Incoming call received. Call ID:", call_id)
+        incoming_call = MyCall(self, call_id)
+        incoming_call.answer()
 
+def atualizar_login_status(status):
+    with open('config.cfg', 'r') as file:
+        lines = file.readlines()
+    if len(lines) < 6:
+        lines.extend(['\n'] * (6 - len(lines)))
+    lines[5] = f'login: {status}\n'
+    with open('config.cfg', 'w') as file:
+        file.writelines(lines)
+
+incoming_call = None
+
+# Função para verificar se uma porta está em uso
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', port)) == 0
 
+# Função para encontrar uma porta disponível
 def find_available_port(start_port):
     port = start_port
     while is_port_in_use(port):
         port += 1
     return port
 
+# Função para criar transporte
 def create_transport():
+    global ep
     ep = pj.Endpoint()
     ep.libCreate()
     ep_cfg = pj.EpConfig()
-    ep_cfg.logConfig.level = 0  # Configuração para desativar todos os logs
-    ep.libInit(ep_cfg)
+    ep_cfg.logConfig.level = 0
+
+    try:
+        ep.libInit(ep_cfg)
+    except pj.Error as e:
+        print(f"Erro na inicialização do endpoint: {e}")
+        return None, None
 
     transport_cfg = pj.TransportConfig()
     transport_cfg.port = find_available_port(5060)
@@ -84,6 +155,7 @@ def create_transport():
 
     return ep, transport
 
+# Função para criar uma conta
 def create_account(ep):
     acc_cfg = pj.AccountConfig()
     acc_cfg.idUri = f"sip:{EXTENSION}@{SIP_SERVER}"
@@ -95,16 +167,65 @@ def create_account(ep):
     for codec in CODECS:
         acc_cfg.codecIds.push_back(codec)
 
-    account = MyAccount()
-    account.create(acc_cfg)
+    try:
+        account = MyAccount()
+        account.create(acc_cfg)
+    except pj.Error as e:
+        print(f"Erro ao criar a conta: {e}")
+        return None
 
     return account
 
+# Função principal para organizar a execução do código
+def main():
+    # Criar transporte
+    global ep
+    ep, transport = create_transport()
+    if not ep or not transport:
+        print("Erro ao criar transporte. Verifique as configurações.")
+        return
+
+    # Criar conta
+    account = create_account(ep)
+    if not account:
+        print("Erro ao criar a conta. Verifique as configurações.")
+        return
+
+    # Fazer chamada de teste
+    destination_number = configuracoes.get('Destination Number')
+    if destination_number:
+        atualizar_estado_ligacao('conectada')
+        call = make_call(account, destination_number)
+        print(f"Chamada para {destination_number} iniciada.")
+    else:
+        print("Número de destino não especificado nas configurações.")
+
+    # Manter o programa em execução para lidar com chamadas
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Finalizando...")
+
+    # Liberação dos recursos do PJSIP
+    account.shutdown()
+    ep.libDestroy()
+
+# Função para fazer uma chamada
 def make_call(account, destination_number):
+    if not account:
+        print("Conta não está registrada. Não é possível fazer chamada.")
+        return
+
     destination = f"sip:{destination_number}@{SIP_SERVER}"
     call = MyCall(account)
     prm = pj.CallOpParam()
     prm.opt.audioCount = 1
     prm.opt.videoCount = 0
     call.makeCall(destination, prm)
+    atualizar_estado_ligacao('conectada')
     return call
+
+# Chamar a função principal
+if __name__ == "__main__":
+    main()
